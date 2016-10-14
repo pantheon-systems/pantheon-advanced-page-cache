2e3f7c9aef6706526be4c9d5240dd42e9a306fc6
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-14 15:30:29 -0700

    Use an autoloader for our classes

--- pantheon-integrated-cdn.php
+++ pantheon-integrated-cdn.php
@@ -12,6 +12,24 @@
  * @package         Pantheon_Integrated_Cdn
  */
 
-require_once dirname( __FILE__ ) . '/inc/class-emitter.php';
+/**
+ * Register the class autoloader
+ */
+spl_autoload_register( function( $class ) {
+	$class = ltrim( $class, '\\' );
+	if ( 0 !== stripos( $class, 'Pantheon_Integrated_CDN\\' ) ) {
+		return;
+	}
+
+	$parts = explode( '\\', $class );
+	array_shift( $parts ); // Don't need "Pantheon_Integrated_CDN"
+	$last = array_pop( $parts ); // File should be 'class-[...].php'
+	$last = 'class-' . $last . '.php';
+	$parts[] = $last;
+	$file = dirname( __FILE__ ) . '/inc/' . str_replace( '_', '-', strtolower( implode( $parts, '/' ) ) );
+	if ( file_exists( $file ) ) {
+		require $file;
+	}
+});
 
 add_filter( 'wp', array( 'Pantheon_Integrated_CDN\Emitter', 'action_wp' ) );
