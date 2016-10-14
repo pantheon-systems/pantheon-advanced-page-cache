7915e0cd515277f5f218a7836912eab32c48a26d
Daniel Bachhuber <daniel@handbuilt.co>
2016-10-14 12:57:47 -0700

    Initial commit of scaffold code

---
+++ pantheon-integrated-cdn.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Pantheon Integrated CDN
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Pantheon
+ * Author URI:      https://pantheon.io
+ * Text Domain:     pantheon-integrated-cdn
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pantheon_Integrated_Cdn
+ */
