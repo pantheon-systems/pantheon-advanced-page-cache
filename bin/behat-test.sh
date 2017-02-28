e2ab5f4a8df5341afeed75c587385625bfd8a222
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-28 07:11:16 -0800

    Use Terminus 1.0 in Circle tests

--- bin/behat-test.sh
+++ bin/behat-test.sh
@@ -4,6 +4,12 @@
 # Execute the Behat test suite against a prepared Pantheon site environment.
 ###
 
+terminus whoami > /dev/null
+if [ $? -ne 0 ]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
+fi
+
 set -ex
 
 ./bin/behat-check-required.sh
