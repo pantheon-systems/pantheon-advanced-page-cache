3bac4231f0d939686e01fa996326814db9f93adc
Chris Reynolds <chris@jazzsequence.com>
2024-05-08 10:30:33 -0600

    don't show notices if wp_admin_notice doesn't exist

--- inc/class-admin-interface.php
+++ inc/class-admin-interface.php
@@ -14,7 +14,12 @@ class Admin_Interface {
 	/**
 	 * Kick off the important bits.
 	 */
-	public function bootstrap() {
+	public static function bootstrap() {
+		// Check if wp_admin_notice exists. We've already noted that the plugin requires at least 6.4, so we're going to not display a notice if you didn't listen to the recommendation.
+		if ( ! function_exists( 'wp_admin_notice' ) ) {
+			add_filter( 'pantheon_apc_disable_admin_notices', '__return_true' );
+		}
+
 		if ( defined( 'PANTHEON_MU_PLUGIN_VERSION' ) ) {
 			// Only do things here if we've got the MU plugin and it's > 1.4.0.
 			if ( version_compare( PANTHEON_MU_PLUGIN_VERSION, '1.4.0', '>' ) ) {
