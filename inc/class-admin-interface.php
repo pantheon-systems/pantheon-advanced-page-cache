bb0c5f15e7f9d0ba4ef25f59cc167d1e261c6df8
Chris Reynolds <chris@jazzsequence.com>
2024-05-08 09:31:42 -0600

    some more scaffolding

--- inc/class-admin-interface.php
+++ inc/class-admin-interface.php
@@ -15,9 +15,15 @@ class Admin_Interface {
 	 * Kick off the important bits.
 	 */
 	public function bootstrap() {
-		// Only do things here if we've got the MU plugin and it's > 1.4.0.
-		if ( defined( 'PANTHEON_MU_PLUGIN_VERSION' ) && version_compare( PANTHEON_MU_PLUGIN_VERSION, '1.4.0', '>' ) ) {
-			// Do stuff, e.g. add_action().
+		if ( defined( 'PANTHEON_MU_PLUGIN_VERSION' ) ) {
+			// Only do things here if we've got the MU plugin and it's > 1.4.0.
+			if ( version_compare( PANTHEON_MU_PLUGIN_VERSION, '1.4.0', '>' ) ) {
+				// Do stuff, e.g. add_action().
+			} else {
+				// admin notice to update the WordPress upstream. maybe detect if this is a composer site and suggest a composer update.
+			}
+		} else {
+			// admin notice that this plugin is designed for use on Pantheon.
 		}
 	}
 }
