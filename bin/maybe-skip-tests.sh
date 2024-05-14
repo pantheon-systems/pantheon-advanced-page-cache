ed5001b2d705e782c7dd731c21b9248c528d9538
Chris Reynolds <chris@jazzsequence.com>
2024-05-14 15:33:20 -0600

    fix the logic (again)

--- bin/maybe-skip-tests.sh
+++ bin/maybe-skip-tests.sh
@@ -25,19 +25,20 @@ ignored_paths=(
 
 # Fetch list of changed files from the last commit
 changed_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
-should_run_tests=true
+should_skip=false
 
 for file in $changed_files; do
-	if ! echo "$file" | grep -Eq "^($ignored_paths)"; then
-	  should_run_tests=true
+	if echo "$file" | grep -Eq "^($ignored_paths)$"; then
+	  should_skip=true
+	  echo "Skipping $file..."
+	else
+	  should_skip=false
 	  echo "Running tests because $file was changed."
 	  break
-	else
-	  should_run_tests=false
 	fi
 done
 
-if [ "$should_run_tests" = false ]; then
+if [ "$should_skip" = true ]; then
   echo "Only ignored files modified. Skipping Behat tests."
   circleci-agent step halt
 fi
