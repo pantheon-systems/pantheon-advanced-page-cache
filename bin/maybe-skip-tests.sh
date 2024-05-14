e40e6a6d35a362864121c32177e8ea53a4cd728a
Chris Reynolds <chris@jazzsequence.com>
2024-05-14 15:08:19 -0600

    add script to maybe skip behat tests if we don't need to run them

---
+++ bin/maybe-skip-tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Paths to ignore.
+ignored_paths=(
+  .editorconfig
+  .gitattributes
+  .gitignore
+  CODEOWNERS
+  CONTRIBUTING.md
+  LICENSE
+  phpcs.xml.dist
+  phpunit.xml.dist
+  README.md
+  readme.txt
+  .wordpress-org/*
+  .github/*
+  .circleci/*
+  tests/phpunit/*
+)
+
+# Fetch list of changed files from the last commit
+changed_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
+should_run_tests=true
+
+for file in $changed_files; do
+	if ! echo "$file" | grep -Eq "^($ignored_paths)"; then
+	  should_run_tests=true
+	  echo "Running tests because $file was changed."
+	  break
+	else
+	  should_run_tests=false
+	fi
+done
+
+if [ "$should_run_tests" = false ]; then
+  echo "Only ignored files modified. Skipping Behat tests."
+  circleci-agent step halt
+fi
