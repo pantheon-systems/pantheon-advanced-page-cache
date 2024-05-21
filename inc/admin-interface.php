358f2ddd80a8af9d4da5f3b9f5e96bc2a4d2f5ee
Chris Reynolds <chris.reynolds@pantheon.io>
2024-05-21 16:27:18 -0600

    move the debug note
    Co-authored-by: Phil Tyler <phil.tyler@pantheon.io>

--- inc/admin-interface.php
+++ inc/admin-interface.php
@@ -57,9 +57,9 @@ function enqueue_admin_assets() {
 		return;
 	}
 
+	// If WP_DEBUG is true, append a timestamp to the end of the path so we get a fresh copy of the css.
 	$debug = defined( 'WP_DEBUG' ) && WP_DEBUG ? '-' . time() : '';
-
-	wp_enqueue_style( 'papc-admin', plugin_dir_url( __DIR__ ) . 'assets/css/styles.css', [], '2.0.0' . $debug ); // If WP_DEBUG is true, append a timestamp to the end of the path so we get a fresh copy of the css.
+	wp_enqueue_style( 'papc-admin', plugin_dir_url( __DIR__ ) . 'assets/css/styles.css', [], '2.0.0' . $debug );
 }
 
 /**
