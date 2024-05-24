349b4d2f3c3fa34ad0b05a3ee797612668fc7def
Chris Reynolds <chris@jazzsequence.com>
2024-05-24 10:22:57 -0600

    update the build workflow to create two css files

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,14 +3,17 @@ import gulp from 'gulp';
 import csso from 'gulp-csso';
 import * as sassCompiler from 'sass';
 import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename'
 
 const sass = gulpSass(sassCompiler);
 
 gulp.task('styles', () => {
   return gulp.src('assets/sass/styles.scss') // Only compile the main file
     .pipe(sass().on('error', sass.logError)) // Compile SASS to CSS
+	.pipe(gulp.dest('assets/css')) // Save unminified CSS
     .pipe(csso()) // Minify CSS
-    .pipe(gulp.dest('assets/css')); // Save the CSS file
+	.pipe(rename({suffix: '.min'})) // Rename to styles.min.css
+    .pipe(gulp.dest('assets/css')); // Save the minified CSS file
 });
 
 gulp.task('clean', () => {
