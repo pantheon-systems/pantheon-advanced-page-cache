917565935fdb76344c76e20f54d04b53729fbe78
Chris Reynolds <chris@jazzsequence.com>
2024-08-05 12:40:27 -0600

    add tests

--- tests/phpunit/test-admin-interface.php
+++ tests/phpunit/test-admin-interface.php
@@ -328,6 +328,14 @@ class Admin_Interface_Functions extends \Pantheon_Advanced_Page_Cache_Testcase {
 			add_filter( 'pantheon_cache_default_max_age', function() {
 				return 10 * DAY_IN_SECONDS;
 			} );
+		} elseif ( $max_age === 'multiple-filters-below' ) {
+			add_filter( 'pantheon_cache_default_max_age', [ $this, 'reset_cache_max_age' ] );
+			add_filter( 'pantheon_cache_default_max_age', [ $this, 'another_function' ] );
+			add_filter( 'pantheon_cache_default_max_age', function() {
+				return 3 * DAY_IN_SECONDS;
+			} );
+		} elseif ( $max_age === 'just-named-filters' ) {
+			add_filter( 'pantheon_cache_default_max_age', [ $this, 'reset_cache_max_age' ] );
 		} else {
 			update_option( 'pantheon-cache', [ 'default_ttl' => $max_age ] );
 		}
@@ -343,8 +351,10 @@ class Admin_Interface_Functions extends \Pantheon_Advanced_Page_Cache_Testcase {
 	 */
 	public function add_max_age_setting_description_provider() {
 		return [
-			[ 'filter-below', 'Your cache maximum age is currently <strong>below</strong> the recommended value. This value has been hardcoded to <strong>3 days</strong> via a filter.' ],
-			[ 'filter-above', 'Your cache maximum age is currently <strong>above</strong> the recommended value. This value has been hardcoded to <strong>1 week</strong> via a filter.' ],
+			[ 'filter-below', 'Your cache maximum age is currently <strong>below</strong> the recommended value. This value has been hardcoded to <strong>3 days</strong> via a filter hooked to an anonymous function in your code.' ],
+			[ 'filter-above', 'Your cache maximum age is currently <strong>above</strong> the recommended value. This value has been hardcoded to <strong>1 week</strong> via a filter hooked to an anonymous function in your code.' ],
+			[ 'multiple-filters-below', 'Your cache maximum age is currently <strong>below</strong> the recommended value. This value has been hardcoded to <strong>3 days</strong> via a filter hooked to <code>Pantheon_Advanced_Page_Cache\Admin_Interface\Admin_Interface_Functions::reset_cache_max_age</code>, <code>Pantheon_Advanced_Page_Cache\Admin_Interface\Admin_Interface_Functions::another_function</code>, and an anonymous function in your code.' ],
+			[ 'just-named-filters', 'Your cache maximum age is currently <strong>below</strong> the recommended value. This value has been hardcoded to <strong>1 second</strong> via a filter hooked to <code>Pantheon_Advanced_Page_Cache\Admin_Interface\Admin_Interface_Functions::reset_cache_max_age<code> in your code.' ],
 			[ 600, 'Your cache maximum age is currently <strong>below</strong> the recommended value. <br /><strong>Warning:</strong>The cache max age is not one of the recommended values.' ],
 			[ WEEK_IN_SECONDS, 'Your cache maximum age is currently set to the recommended value.' ],
 			[ MONTH_IN_SECONDS, 'Your cache maximum age is currently <strong>above</strong> the recommended value.' ],
